6631b9dce71343ceba8f656c95bde85effa89ed5
Filipe <89738919+fpfrances@users.noreply.github.com>
2025-04-26 15:10:38 -0400

    Finished types of coffee and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,55 @@
-import React from 'react';
 import './about.css';
+import React, { useState, useEffect } from 'react';
+import Coffee1 from '../assets/cheiro.jpg';
+import Coffee2 from '../assets/colheita.jpg';
+import Coffee3 from '../assets/graos.jpg';
+import Coffee4 from '../assets/love.jpg';
 
 function Historia() {
+  const [currentImage, setCurrentImage] = useState(Coffee1); // State to store the current image
+
+  // Array of image sources
+  const images = [Coffee1, Coffee2, Coffee3, Coffee4];
+
+  // Function to rotate images
+  const rotateImages = () => {
+    setCurrentImage((prevImage) => {
+      const currentIndex = images.indexOf(prevImage);
+      const nextIndex = (currentIndex + 1) % images.length;
+      return images[nextIndex];
+    });
+  };
+
+  // Rotate images every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(rotateImages, 5000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  },);
+
   return (
-  <div className="about-container">A “We Love Coffee” tem por missão levar a nossos clientes uma experiência única dos diferentes sabores de cafés das regiões do Brasil.
-  Assim, buscamos os melhores grãos no mercado, com foco na qualidade, para levar o melhor café especial, desde o pé de café até sua xícara.
-  Nossa Torrefação tem em sua linha de produção, um dos melhores Torradores do mercado, sendo a nossa torra, um processo artesanal e único, desenvolvido com foco na qualidade, capaz de agradar aos mais exigentes degustadores de cafés especiais do Brasil e do mundo.
-  Não deixe de fazer parte dessa experiência fantástica, com a família “We Love Coffee”.
-  </div>
+    <div className="container">
+      {/* Rotating Images */}
+      <div className="rotating-images">
+        <img src={currentImage} className="App-bags" alt="Coffee Bag" />
+      </div>
+      {/* Message Container */}
+    <div className="message-container">
+      <h2>Nossa Historia</h2>
+        <p>Sempre me encantei com o sabor que o café poderia proporcionar, seja em uma receita mais elaborada, no leite ou simplesmente a xícara de café puro.
+            A cultura do café está profundamente enraizada no DNA brasileiro! Fui apresentado ao universo dos cafés especiais pelo meu amigo Francisco, um especialista em torra de café especial.<br></br>
+            A partir desse momento, comecei a enfrentar dificuldades para encontrar cafés especiais com torra fresca e personalizados!<br></br>
+            Para satisfazer uma demanda cada vez mais exigente e em crescimento. Desde 2018, tenho me dedicado ao estudo de cafés especiais no Brasil e globalmente.
+            Desde a plantação, colheita, secagem, até a torra e moagem, até chegar à sua xícara.<br></br>
+            Especializei-me:<br></br>
+            - Classificação e degustação de cafés (verde e torrado).<br></br>
+            - Especialista em torra de cafés.<br></br>
+            - Barista de nível avançado.<br></br>
+            Hoje, tenho uma pequena torrefação de cafés especiais artesanais, a We Love Coffee, que foi fundada em 2019 com foco na qualidade
+            e atendimento ao cliente. Temos sempre ótimos cafés à sua espera!</p>
+    </div>
+    </div>
 );}
 
 export default Historia;
--- src/pages/types.js
+++ src/pages/types.js
@@ -2,7 +2,92 @@ import React from 'react';
 import './types.css';
 
 function Cafes() {
-  return <div>Welcome to the Home Page! Learn about our coffee types here.</div>;
+  return (
+    <div className='container-wrap'>
+      <h1>Nossos tipos de cafés</h1>
+      <p1>Para se obter a melhor qualidade dos cafés artesanais, as nossas torras de cafés sao sobre encomenda para que os nossos clientes recebam os seus cafés frescos. Assim os clientes poderão degustar de um café delicioso e com alta qualidade.</p1>
+      <div className='container-cafes'>
+        <div className='container-cafes-item'>
+        <div class="cafe-card">
+        <h3>Doce Caparaó</h3>
+          <p>Regiao: Caparaó/MG<br></br>
+            Propriedade: Sítio Santa Rita<br></br>
+            Cafeicultor: Tarcisio Lacerda(Pai) e Jhone Lacerda(Filho)<br></br>
+            Espécie: Arábica<br></br>
+            Variedade: Catuaí Vermelho<br></br>
+            Processo: Natural<br></br>
+            Altitude: 1250mts
+          </p>
+        </div>
+        </div>
+        <div className='container-cafes-item'>
+        <div class="cafe-card">
+        <h3>Jasmin</h3>
+          <p>Regiao: Caparaó/MG<br></br>
+            Propriedade: Sítio Santa Rita<br></br>
+            Cafeicultor: Tarcisio Lacerda(Pai) e Jhone Lacerda(Filho)<br></br>
+            Espécie: Arábica<br></br>
+            Variedade: Caturra / Caparaó Amarelo<br></br>
+            Processo: Natural<br></br>
+            Altitude: 1430 mts
+          </p>
+        </div>
+        </div>
+        <div className='container-cafes-item'>
+        <div class="cafe-card">
+        <h3>Flor de Laranjeira</h3>
+          <p>Regiao: Caparaó/MG<br></br>
+            Propriedade: Sítio Santa Rita<br></br>
+            Cafeicultor: Tarcisio Lacerda(Pai) e Jhone Lacerda(Filho)<br></br>
+            Espécie: Arábica<br></br>
+            Variedade: Caturra / Caparaó Amarelo<br></br>
+            Processo: Honey<br></br>
+            Altitude: 1430 mts
+          </p>
+        </div>
+        </div>
+        <div className='container-cafes-item'>
+        <div class="cafe-card">
+        <h3>Spicy</h3>
+          <p>Regiao: Mantiqueira/MG<br></br>
+            Propriedade: Sítio Boa Vista<br></br>
+            Cafeicultor: Maria Rosana Reis de Souza<br></br>
+            Espécie: Arábica<br></br>
+            Variedade: Mundo Novo<br></br>
+            Processo: Natural<br></br>
+            Altitude: 1150 mts
+          </p>
+        </div>
+        </div>
+        <div className='container-cafes-item'>
+        <div class="cafe-card">
+        <h3>Flor de Fruta</h3>
+          <p>Regiao: Alto Caparaó/MG<br></br>
+            Propriedade: Sítio Pinheiro Silvestre<br></br>
+            Cafeicultor: Cléufa de Fátima Pinheiro Costa Silvestre<br></br>
+            Espécie: Arábica<br></br>
+            Variedade: Catuaí Vermelho<br></br>
+            Processo: Honey<br></br>
+            Altitude: 1350 mts
+          </p>
+        </div>
+        </div>
+        <div className='container-cafes-item'>
+        <div class="cafe-card">
+        <h3>Kana</h3>
+          <p>Regiao: Alto Caparaó/MG<br></br>
+            Propriedade: Sítio Pinheiro Silvestre<br></br>
+            Cafeicultor: Cléufa de Fátima Pinheiro Costa Silvestre<br></br>
+            Espécie: Arábica<br></br>
+            Variedade: Catuaí Vermelho<br></br>
+            Processo: Natural<br></br>
+            Altitude: 1250 mts
+          </p>
+        </div>
+        </div>
+        </div>
+    </div>
+  );
 }
 
 export default Cafes;
