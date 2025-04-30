1578a949047f3058bfdffb7d890fe2f0a4a463e2
Filipe <89738919+fpfrances@users.noreply.github.com>
2025-04-30 09:48:34 -0400

    404 page not found issue

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const redirectPath = sessionStorage.redirect;
+if (redirectPath) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirectPath);
+}
+
 root.render(
   <React.StrictMode>
     <App />
